c5ea58192b583549034a7af2c89937a0bbd71ec1
gujingjing <1546804319@qq.com>
2016-07-25 17:44:01 +0800

    test1.2

---
+++ compliteTest/libtest/src/main/java/com/example/libtest/Method.java
@@ -0,0 +1,12 @@
+package com.example.libtest;
+
+/**
+ * 作者：iBesteeth on 2016/7/25 16:52
+ * 邮箱：gujingjing@ibesteeth.com
+ */
+
+public class Method {
+    public static String getName(){
+        return "gjj";
+    }
+}
