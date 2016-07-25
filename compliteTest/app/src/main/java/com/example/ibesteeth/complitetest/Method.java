709acfa8239ad90f4526ba9287a02642b31e9ad9
gujingjing <1546804319@qq.com>
2016-07-25 16:27:06 +0800

    complite测试

---
+++ compliteTest/app/src/main/java/com/example/ibesteeth/complitetest/Method.java
@@ -0,0 +1,12 @@
+package com.example.ibesteeth.complitetest;
+
+/**
+ * 作者：iBesteeth on 2016/7/25 16:20
+ * 邮箱：gujingjing@ibesteeth.com
+ */
+
+public class Method {
+    public static String getName(){
+        return "gjj_test";
+    }
+}
